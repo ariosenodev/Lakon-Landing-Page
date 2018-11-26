3805e1a8aa9bbeb46b94c8a68d8c579d8fc61544
ariosenodev <45356321+ariosenodev@users.noreply.github.com>
2018-11-26 17:57:36 +0700

    Add files via upload

---
+++ dev-lakon/assets/js/main.js
@@ -0,0 +1,7 @@
+// navbar changed when scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
